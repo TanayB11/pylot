c594a4431b9ba04e5b031d15941391e29bb6c9cd
Tanay Biradar <34867487+TanayB11@users.noreply.github.com>
2023-04-07 12:04:21 -0700

    Add inference code

---
+++ src/ppo_inference.py
@@ -0,0 +1,121 @@
+import argparse
+import os
+import random
+import time
+from distutils.util import strtobool
+
+# import gymnasium as gym
+import gym
+import gym_jsbsim
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.normal import Normal
+from torch.utils.tensorboard import SummaryWriter
+
+from ppo_continuous_action import make_env, layer_init, Agent
+
+def parse_args():
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
+        help="the name of this experiment")
+    parser.add_argument("--seed", type=int, default=1,
+        help="seed of the experiment")
+    parser.add_argument("--torch-deterministic", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="if toggled, `torch.backends.cudnn.deterministic=False`")
+    parser.add_argument("--cuda", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="if toggled, cuda will be enabled by default")
+    parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="whether to capture videos of the agent performances (check out `videos` folder)")
+
+    parser.add_argument("--model-path", type=str, default="", 
+        help="path to the .cleanrl_model file from training")
+
+    # Algorithm specific arguments
+    parser.add_argument("--env-id", type=str, default="JSBSim-TurnHeadingControlTask-Cessna172P-Shaping.STANDARD-NoFG-v0",
+        help="the id of the environment")
+
+    parser.add_argument("--num-steps", type=int, default=2048,
+        help="the number of steps to run in each environment per policy rollout")
+    parser.add_argument("--total-timesteps", type=int, default=100000,
+        help="total timesteps of the experiments")
+    parser.add_argument("--gamma", type=float, default=0.99,
+        help="the discount factor gamma")
+
+    args = parser.parse_args()
+    args.num_envs = 1 # for inference
+    args.batch_size = int(args.num_envs * args.num_steps)
+    # args.minibatch_size = int(args.batch_size // args.num_minibatches)
+    # fmt: on
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    run_name = f"INFERENCE_{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+
+    print(args)
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+
+    # env setup
+    envs = gym.vector.SyncVectorEnv(
+        [make_env(args.env_id, i, args.capture_video, run_name, args.gamma) for i in range(args.num_envs)]
+    )
+    assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
+
+    agent = Agent(envs).to(device)
+    agent.load_state_dict(torch.load(args.model_path))
+    agent.eval()
+
+
+    # ALGO Logic: Storage setup
+    obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
+    actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
+    logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    values = torch.zeros((args.num_steps, args.num_envs)).to(device)
+
+    # TRY NOT TO MODIFY: start the game
+    global_step = 0
+    start_time = time.time()
+    next_obs = envs.reset()
+    next_obs = torch.Tensor(next_obs).to(device)
+    next_done = torch.zeros(args.num_envs).to(device)
+    num_updates = args.total_timesteps // args.batch_size
+    video_filenames = set()
+
+    # run inference loop
+    for step in range(0, args.num_steps):
+        global_step += 1 * args.num_envs
+        obs[step] = next_obs
+        dones[step] = next_done
+
+        # ALGO LOGIC: action logic
+        with torch.no_grad():
+            action, logprob, _, value = agent.get_action_and_value(next_obs)
+            values[step] = value.flatten()
+        actions[step] = action
+        logprobs[step] = logprob
+
+        # TRY NOT TO MODIFY: execute the game and log data.
+        next_obs, reward, done, infos = envs.step(action.cpu().numpy())
+        rewards[step] = torch.tensor(reward).to(device).view(-1)
+        next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(done).to(device)
+
+        # print(reward, done, infos)
+
+        if done:
+            print(f"global_step={global_step}, episodic_return={infos[0]['episode']['r']}")
+            break
+
+
